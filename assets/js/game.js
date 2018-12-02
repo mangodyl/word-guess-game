d3047767a46517fed818894815cbae17820c868e
mango dyl <dylanbonds@hotmail.com>
2018-12-01 19:06:46 -0600

    js work

---
+++ assets/js/game.js
@@ -0,0 +1,92 @@
+
+
+// create Array of words
+
+// set variables for:
+    // wrong guesses []
+    // correct guesses []
+    // max wrong guesses
+    // current array index
+    // wins
+    // losses
+
+// Array of possible words
+var wordArray = [
+    "coldplay",
+    "weezer",
+    "queen",
+    "prince",
+    "nirvana",
+    "feeder",
+    "gorillaz",
+    "jamiroquai",
+    "muse",
+    "outkast",
+    "pixies"
+]
+
+// Setting variables
+var wrongGuesses = [];
+var answerArray = [];
+var maxWrongGuesses = 10;
+var randomIndex;
+var wordIndex;
+var wins = 0;
+var losses = 0;
+var userGuess = [];
+
+
+// function on reset:
+    // random array index chosen
+    // wins & losses set to 0
+    // empty wrongGuesses & correctGuesses arrays
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+        
+    randomIndex = Math.floor(Math.random() * wordArray.length);
+    wordIndex = wordArray[randomIndex];
+    console.log(wordIndex);
+
+    // wins and losses
+
+    // --------------- document.getElementById("#winSpan").textContent = wins;
+
+    // Use random word length to determine number of underscores.
+
+    for (var i = 0; i < wordIndex.length; i++) {
+        answerArray[i] = "_";
+    };
+    
+    var stringArray = answerArray.join(" ");
+
+    document.getElementById("activeWord").innerHTML = stringArray;
+
+    });
+
+// function to make sure key presses are letters
+
+document.onkeydown = function(event) {
+
+    var userGuess = event.key;
+
+    if (event.keyCode >= 65 && event.keyCode <= 90) {
+        makeGuess(event.key.toLocaleLowerCase())
+    };
+
+};
+
+
+// function for making the guess
+
+function makeGuess(letter) {
+
+    if (userGuess.indexOf(letter) === -1) {
+        console.log(letter);
+        //userGuess.push(letter);
+        //evaluateGuess(letter);
+    };
+};
+
+
+
+
